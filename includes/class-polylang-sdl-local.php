906a9205b0a25302bb059ffc45fa0a71961824ac
fightwithcrayon <contact@andrewthomashill.co.uk>
2017-03-09 15:38:06 +0000

    Local: Functions for updating DB with downloaded translations
    * Create new posts/update as appropriate
    * Create new taxonomy terms/meta or update existing name as appropriate
    * Update Polylang language database to connect translations and
    originals

---
+++ includes/class-polylang-sdl-local.php
@@ -0,0 +1,114 @@
+<?php
+
+class Polylang_SDL_Local {
+
+	private $verbose = true;
+    private $post_structure;
+    private $syslangs;
+    public function __construct() {
+        $this->syslangs = pll_languages_list();
+    }
+
+    private function verbose($msg) {
+    	if($this->verbose === true) {
+    		echo '<b>Console: </b>'. $msg .'<br />';
+    	}
+    }
+    public function save_post_translation($structure){
+        $this->post_structure = $structure;
+        $existing_id = pll_get_post($structure['id'], $structure['attributes']['target-language']);
+        return $this->update_translation($existing_id);
+    }
+    public function get_post_translations($post_id){
+        $results = array();
+        foreach($this->syslangs as $lang) {
+            $results[$lang] = pll_get_post($post_id, $lang);
+        }
+        return $results;
+    }
+    public function get_term_translations($term_id){
+        $results = array();
+        foreach($this->syslangs as $lang) {
+            $results[$lang] = pll_get_term($term_id, $lang);
+        }
+        return $results;
+    }
+
+    private function update_translation($id){
+        if($id === null || $id === false) {
+            $id = $this->create_translation_post();
+        } else {
+            $this->update_translation_post($id);
+        }
+        foreach($this->post_structure['taxonomy'] as $tax => $terms) {
+            $trans_terms = $this->save_taxonomy_translations($tax, $terms);
+            $this->post_structure['translated_taxonomies'][$tax] = $trans_terms;
+        }
+        $this->attach_taxonomy_translations($this->post_structure['translated_taxonomies'], $id);
+        $this->save_meta_translations($this->post_structure['meta'], $id);
+        return $id;
+    }
+    private function create_translation_post(){
+        $args = array(
+            'post_title' => $this->post_structure['title'],
+            'post_content' => $this->post_structure['body'],
+            'post_type' => get_post_type($this->post_structure['id']),
+            'post_status' => get_post_status($this->post_structure['id']),
+            'post_author' => get_the_author_meta($this->post_structure['id']),
+            );
+        $id = wp_insert_post($args);
+        pll_set_post_language($id, $this->post_structure['attributes']['target-language']);
+
+        $translations = $this->get_post_translations($this->post_structure['id']);
+        $translations[$this->post_structure['attributes']['target-language']] = $id;
+        pll_save_post_translations($translations);
+
+        return $id;
+    }
+    private function update_translation_post($id){
+        $args = array(
+            'ID' => $id,
+            'post_title' => $this->post_structure['title'],
+            'post_content' => $this->post_structure['body']
+            );
+        $id = wp_update_post($args);
+        return $id;
+    }
+    private function save_taxonomy_translations($tax, $terms){
+        $results = array();
+        foreach($terms as $id => $name) {
+            $langmap = $this->get_term_translations($id);
+            $target_id = $langmap[$this->post_structure['attributes']['target-language']];
+            //Does term already exist in target language (ie, do we need to register a new term, or just update)
+            if($target_id === null || $target_id === false) {
+                $source = get_term( $id, $tax);
+                $target_id = wp_insert_term($name, $tax, array(
+                    'slug' => $source->slug . '–' . $this->post_structure['attributes']['target-language']
+                    ));
+                pll_set_term_language($target_id['term_id'], $this->post_structure['attributes']['target-language']);
+                $langmap[$this->post_structure['attributes']['target-language']] =  $target_id['term_id'];
+                pll_save_term_translations($langmap);
+                $final_id = $target_id['term_id'];
+            } else {
+                wp_update_term($target_id, $tax, array(
+                    'name' => $name
+                ));
+                $final_id = $target_id;
+            }
+            $results[] = $final_id;
+        }
+        return $results;
+    }
+    private function attach_taxonomy_translations($taxonomies, $id) {
+        foreach($taxonomies as $tax => $terms) {
+            wp_set_post_terms($id, $terms, $tax);
+        }
+    }
+    private function save_meta_translations($meta, $id){
+        foreach($meta as $name => $value) {
+            update_post_meta($id, $name, $value);
+        }
+    }
+}
+
+?>
