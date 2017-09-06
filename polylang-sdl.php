b2e0178c1fe043582ca33c0d4ba172a11f94612a
fightwithcrayon <contact@andrewthomashill.co.uk>
2017-09-06 18:34:54 +0100

    Add license, remove final debug setting

--- polylang-sdl.php
+++ polylang-sdl.php
@@ -1,13 +1,29 @@
 <?php
 
-/**
+/*
  * @wordpress-plugin
- * Plugin Name:       SDL Managed Translation
+ * Plugin Name:       SDL Managed Translation for Polylang
  * Plugin URI:        http://languagecloud.sdl.com/
  * Description:       SDL Managed Translation integration for Poylang, for translating WordPress site content.
  * Version:           1.0.0
  * Author:            SDL
  * Text Domain:		  managedtranslation
+ * License:     GPL3
+
+ Copyright (C) 2017  SDL
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 // If this file is called directly, abort.
